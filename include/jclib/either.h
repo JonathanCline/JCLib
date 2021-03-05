38d112bc08b19c5c0273f9e2db08aff10d12be51
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-03-05 14:26:15 -0600

    Renamed either types to reflect a first / second semantic usage

--- include/jclib/either.h
+++ include/jclib/either.h
@@ -9,15 +9,61 @@
 namespace jc
 {
 	
-	template <typename T, typename U>
+	template <typename FirstT, typename SecondT>
 	class either
 	{
+	private:
+		enum : bool
+		{
+			isFirstType = false,
+			isSecondType = true
+		};
+
 	public:
+		using first_type = FirstT;
+		using second_type = SecondT;
+
+		struct second_value_t {};
+		JCLIB_CONSTANT second_value_t second_value{};
+
+		JCLIB_CONSTEXPR bool has_first() const noexcept
+		{
+			return this->type_ == isFirstType;
+		};
+
+		first_type& first()
+		{
+			
+		};
 
 
 
+
+		JCLIB_CONSTEXPR either(first_type _fval);
+		JCLIB_CONSTEXPR either(second_type _sval);
+
+
+		~either()
+		{
+			if (this->type_ == isFirstType)
+			{
+				this->ft_.~first_type();
+			}
+			else
+			{
+				this->st_.~second_type();
+			};
+		};
 
 	private:
+		
+		bool type_;
+		union
+		{
+			first_type ft_;
+			second_type st_;
+		};
+
 
 	};
 
--- tests/either/test.cpp
+++ tests/either/test.cpp
@@ -3,7 +3,9 @@
 
 #include <string>
 
+
 int main()
 {
-
+	jc::either<int, std::string> _f{ 3 };
+	return 0;
 };
