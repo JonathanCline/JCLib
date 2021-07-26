d4256f885c1051af9318365815d993b294b632cb
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-07-25 20:49:34 -0500

    Added copyright notice to exception.h

--- include/jclib/exception.h
+++ include/jclib/exception.h
@@ -2,9 +2,23 @@
 #ifndef JCLIB_EXCEPTION_H
 #define JCLIB_EXCEPTION_H
 
+/*
+	Copyright 2021 Jonathan Cline
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+	(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+	publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+	so, subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+	WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "jclib/config.h"
 
 #include <exception>
+#include <new>
 
 #define _JCLIB_EXCEPTION_
 
@@ -13,12 +27,12 @@ namespace jc
 	/**
 	 * @brief Tag type for non-throwing function overloads
 	*/
-	struct nothrow_t {};
+	using std::nothrow_t;
 
 	/**
 	 * @brief Tag value type for non-throwing function overloads
 	*/
-	constexpr static nothrow_t nothrow{};
+	using std::nothrow;
 	
 	/**
 	 * @brief Base exception type alias
