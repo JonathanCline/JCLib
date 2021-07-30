55d26fad4bb5d9a1b9b3f5217f2aacea96e56ff6
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-07-30 11:56:20 -0500

    Added comments to function objects

--- include/jclib/functional.h
+++ include/jclib/functional.h
@@ -35,6 +35,12 @@ namespace jc
 	{
 		return _op(std::forward<ArgTs>(_args)...);
 	};
+	
+	/**
+	 * @brief Invokes a function or invocable object and returns its result
+	 * @param _op Function or invocable object to invoke
+	 * @param _args... Arguements to pass as the parameters
+	*/
 	template <typename ReturnT, typename... ArgTs>
 	JCLIB_CONSTEXPR inline auto invoke(ReturnT(*_fn)(ArgTs...), ArgTs&&... _args)
 		-> ReturnT
@@ -42,6 +48,12 @@ namespace jc
 		return (*_fn)(std::forward<ArgTs>(_args)...);
 	};
 #ifdef __cpp_noexcept_function_type
+
+	/**
+	 * @brief Invokes a function or invocable object and returns its result
+	 * @param _op Function or invocable object to invoke
+	 * @param _args... Arguements to pass as the parameters
+	*/
 	template <typename ReturnT, typename... ArgTs>
 	JCLIB_CONSTEXPR inline auto invoke(ReturnT(*_fn)(ArgTs...) noexcept, ArgTs&&... _args) noexcept
 		-> ReturnT
@@ -50,12 +62,23 @@ namespace jc
 	};
 #endif
 
+	/**
+	 * @brief Invokes a function or invocable object and returns its result
+	 * @param _op Function or invocable object to invoke
+	 * @param _args... Arguements to pass as the parameters
+	*/
 	template <typename ReturnT, typename ClassT, typename... ArgTs>
 	JCLIB_CONSTEXPR inline auto invoke(ReturnT(ClassT::*_op)(ArgTs...), ClassT* _class, ArgTs&&... _args)
 		-> ReturnT
 	{
 		return (_class->*_op)(std::forward<ArgTs>(_args)...);
 	};
+
+	/**
+	 * @brief Invokes a function or invocable object and returns its result
+	 * @param _op Function or invocable object to invoke
+	 * @param _args... Arguements to pass as the parameters
+	*/
 	template <typename ReturnT, typename ClassT, typename... ArgTs>
 	JCLIB_CONSTEXPR inline auto invoke(ReturnT(ClassT::* _op)(ArgTs...) const, const ClassT* _class, ArgTs&&... _args)
 		-> ReturnT
@@ -63,12 +86,23 @@ namespace jc
 		return (_class->*_op)(std::forward<ArgTs>(_args)...);
 	};
 #ifdef __cpp_noexcept_function_type
+	/**
+	 * @brief Invokes a function or invocable object and returns its result
+	 * @param _op Function or invocable object to invoke
+	 * @param _args... Arguements to pass as the parameters
+	*/
 	template <typename ReturnT, typename ClassT, typename... ArgTs>
 	JCLIB_CONSTEXPR inline auto invoke(ReturnT(ClassT::* _op)(ArgTs...) noexcept, ClassT* _class, ArgTs&&... _args)
 		noexcept-> ReturnT
 	{
 		return (_class->*_op)(std::forward<ArgTs>(_args)...);
 	};
+
+	/**
+	 * @brief Invokes a function or invocable object and returns its result
+	 * @param _op Function or invocable object to invoke
+	 * @param _args... Arguements to pass as the parameters
+	*/
 	template <typename ReturnT, typename ClassT, typename... ArgTs>
 	JCLIB_CONSTEXPR inline auto invoke(ReturnT(ClassT::* _op)(ArgTs...) const noexcept, const ClassT* _class, ArgTs&&... _args)
 		noexcept-> ReturnT
@@ -91,12 +125,15 @@ namespace jc
 		};
 	};
 
+
 	template <typename Op, template <typename... ArgTs> typename T, typename... ArgTs>
 	constexpr inline auto apply(Op _op, const T<ArgTs...>& _args) ->
 		decltype(jc::invoke(std::declval<Op>(), std::declval<ArgTs>()...))
 	{
 		return impl::apply_impl(std::move(_op), _args, std::make_index_sequence<sizeof...(ArgTs)>());
 	};
+	
+	
 	template <typename Op, template <typename... ArgTs> typename T, typename... ArgTs>
 	constexpr inline auto apply(Op _op, T<ArgTs...>&& _args) ->
 		decltype(jc::invoke(std::declval<Op>(), std::declval<ArgTs>()...))
@@ -256,6 +293,8 @@ namespace jc
 	struct placeholder_t {};
 	constexpr static placeholder_t placeholder{};
 	
+
+
 	template <typename OpT, typename T>
 	constexpr inline auto bind(OpT&& _op, placeholder_t, T&& _value) -> enable_if_t<is_binary_operator<OpT>::value,
 		impl::bound_op<impl::bind_second_t, remove_reference_t<OpT>, remove_reference_t<T>>>
@@ -272,7 +311,9 @@ namespace jc
 		return output_type{ std::forward<OpT>(_op), std::forward<T>(_value) };
 	};
 
-
+	/**
+	 * @brief Less than "<" comparison function object type
+	*/
 	struct less_t : binary_operator<less_t>
 	{
 		template <typename T, typename U>
@@ -282,8 +323,15 @@ namespace jc
 			return lhs < rhs;
 		};
 	};
+
+	/**
+	 * @brief Less than "<" comparison function object
+	*/
 	constexpr static less_t less{};
 
+	/**
+	 * @brief Greater than ">" comparison function object type
+	*/
 	struct greater_t : binary_operator<greater_t>
 	{
 		template <typename T, typename U>
@@ -293,8 +341,15 @@ namespace jc
 			return lhs > rhs;
 		};
 	};
+
+	/**
+	 * @brief Greater than ">" comparison function object
+	*/
 	constexpr static greater_t greater{};
 
+	/**
+	 * @brief Equality "==" comparison function object
+	*/
 	struct equals_t : binary_operator<equals_t>
 	{
 		template <typename T, typename U>
@@ -306,6 +361,9 @@ namespace jc
 	};
 	constexpr static equals_t equals{};
 
+	/**
+	 * @brief Inequality "!=" comparison function object type
+	*/
 	struct unequals_t : binary_operator<unequals_t>
 	{
 		template <typename T, typename U>
@@ -315,19 +373,33 @@ namespace jc
 			return lhs != rhs;
 		};
 	};
+
+	/**
+	 * @brief Inequality "!=" comparison function object
+	*/
 	constexpr static unequals_t unequals{};
 
+	/**
+	 * @brief Addition "+" arithmatic function object type
+	*/
 	struct plus_t : binary_operator<plus_t>
 	{
 		template <typename T, typename U>
-		constexpr inline auto operator()(const T& lhs, const U& rhs) const noexcept ->
-			decltype(std::declval<const T&>() + std::declval<const U&>())
+		constexpr inline auto operator()(T&& lhs, U&& rhs) const noexcept ->
+			decltype(std::declval<T&&>() + std::declval<U&&>())
 		{
 			return lhs + rhs;
 		};
 	};
+
+	/**
+	 * @brief Addition "+" arithmatic function object
+	*/
 	constexpr static plus_t plus{};
 
+	/**
+	 * @brief Subtraction "-" arithmatic function object type
+	*/
 	struct minus_t : binary_operator<minus_t>
 	{
 		template <typename T, typename U>
@@ -337,8 +409,15 @@ namespace jc
 			return lhs - rhs;
 		};
 	};
+
+	/**
+	 * @brief Subtraction "-" arithmatic function object
+	*/
 	constexpr static minus_t minus{};
 
+	/**
+	 * @brief Multiplcation "*" arithmatic function object type
+	*/
 	struct times_t : binary_operator<times_t>
 	{
 		template <typename T, typename U>
@@ -348,8 +427,15 @@ namespace jc
 			return lhs * rhs;
 		};
 	};
+	
+	/**
+	 * @brief Multiplcation "*" arithmatic function object
+	*/
 	constexpr static times_t times{};
 
+	/**
+	 * @brief Division "/" arithmatic function object type
+	*/
 	struct divide_t : binary_operator<divide_t>
 	{
 		template <typename T, typename U>
@@ -359,8 +445,15 @@ namespace jc
 			return lhs / rhs;
 		};
 	};
+
+	/**
+	 * @brief Division "/" arithmatic function object
+	*/
 	constexpr static divide_t divide{};
 
+	/**
+	 * @brief Inversion "!" arithmatic function object type
+	*/
 	struct invert_t : unary_operator_tag
 	{
 		template <typename T>
@@ -369,8 +462,15 @@ namespace jc
 			return !_value;
 		};
 	};
+
+	/**
+	 * @brief Inversion "!" arithmatic function object
+	*/
 	constexpr static invert_t invert{};
 
+	/**
+	 * @brief Binary AND "&&" arithmatic function object type
+	*/
 	struct conjunct_t : binary_operator<conjunct_t>
 	{
 		template <typename T, typename U>
@@ -379,6 +479,10 @@ namespace jc
 			return lhs && rhs;
 		};
 	};
+
+	/**
+	 * @brief Binary AND "&&" arithmatic function object
+	*/
 	constexpr static conjunct_t conjunct{};
 
 
@@ -404,25 +508,54 @@ namespace jc
 		> : true_type {};
 	};
 
-	template <typename Op, typename T, typename U = T, typename = void>
+	/**
+	 * @brief Type trait for checking if an operator can be used with a type or types
+	 * @tparam Op Operator function object type
+	 * @tparam T Left hand side type
+	 * @tparam U Right hand side type
+	 * @tparam Enable SFINAE specialization point
+	*/
+	template <typename Op, typename T, typename U = T, typename Enable = void>
 	struct has_operator : false_type {};
-
+	
+	/**
+	* @brief Type trait for checking if a unary operator can be used with a type
+	* @tparam Op Unary operator function object type
+	* @tparam T Type to use with the operator
+	*/
 	template <typename Op, typename T>
 	struct has_operator<Op, T, T, enable_if_t<
 		is_unary_operator<Op>::value && impl::has_unary_operator<Op, T>::value
 		>> : true_type{};
 
+	/**
+	* @brief Type trait for checking if a binary operator can be used with a type
+	* @tparam Op Binary operator function object type
+	* @tparam T Type to use on both sides of the operator
+	*/
 	template <typename Op, typename T>
 	struct has_operator<Op, T, T, enable_if_t<
 		is_binary_operator<Op>::value && impl::has_binary_operator<Op, T, T>::value
 		>> : true_type{};
 
+	/**
+	* @brief Type trait for checking if a binary operator can be used with two types
+	* @tparam Op Binary operator function object type
+	* @tparam T Left hand side type
+	* @tparam U Right hand side type
+	*/
 	template <typename Op, typename T, typename U>
 	struct has_operator<Op, T, U, enable_if_t<
 		is_binary_operator<Op>::value&& impl::has_binary_operator<Op, T, U>::value
 		>> : true_type{};
 
 #ifdef JCLIB_FEATURE_INLINE_VARIABLES
+	/**
+	 * @brief Convenience variable for the has_operator type trait
+	 * @tparam Op Operator function object type
+	 * @tparam T Left hand side type
+	 * @tparam U Right hand side type
+	*/
 	template <typename Op, typename T, typename U = T>
 	constexpr inline bool has_operator_v = has_operator<Op, T, U>::value;
 #endif
