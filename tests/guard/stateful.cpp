e3fb076dc5d57c4eda54f4e5f388409e3ce319e4
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-09-02 22:34:04 -0500

    Rewrote the guard to allow stateful guarded things

---
+++ tests/guard/stateful.cpp
@@ -0,0 +1,123 @@
+#include <jclib-test.hpp>
+
+#include <jclib/guard.h>
+
+
+struct GuardedState
+{
+	// Set once reset is called
+	bool reset_called = false;
+
+	// Set once set_state is called
+	
+	bool set_state_dead_called = false;
+	bool set_state_alive_called = false;
+
+
+	// Set once get_state is called
+	bool get_state_called = false;
+};
+
+
+
+struct GuardedObject
+{
+	void reset()
+	{
+		this->gstate->reset_called = true;
+	};
+
+	void set_state(jc::guard_state _state)
+	{
+		if (_state == jc::guard_state::dead)
+		{
+			if (this->gstate)
+			{
+				this->gstate->set_state_dead_called;
+			};
+			this->gstate = nullptr;
+		}
+		else
+		{
+			this->gstate->set_state_alive_called = true;
+		};
+	};
+	jc::guard_state get_state() const
+	{
+		if (this->gstate)
+		{
+			this->gstate->get_state_called = true;
+			return jc::guard_state::alive;
+		}
+		else
+		{
+			return jc::guard_state::dead;
+		};
+	};
+
+
+
+
+
+	GuardedObject(GuardedState& _obj) :
+		gstate{ &_obj }
+	{};
+
+	GuardedState* gstate;
+};
+
+using guard_type = jc::guard<GuardedObject>;
+
+
+
+// Test guard.release()
+int subtest_release()
+{
+	NEWTEST();
+
+	GuardedState _state{};
+
+	{
+		guard_type _g{ _state };
+		_g.release();
+
+		ASSERT(_state.set_state_dead_called, "release did not set state to dead");
+		ASSERT(!_state.reset_called, "release called reset");
+	};
+
+	ASSERT(!_state.reset_called, "released guard still called reset after destruction");
+
+	PASS();
+};
+
+// Test guard construction behavior
+int subtest_construction()
+{
+	NEWTEST();
+
+	// Test construction
+	{
+		GuardedState _state{};
+		guard_type _g{ _state };
+		ASSERT(_state.set_state_alive_called, "not set to alive on construction");
+		ASSERT(!_state.set_state_dead_called, "set to dead at construction");
+	};
+
+	PASS();
+};
+
+
+
+
+
+
+
+int main()
+{
+	NEWTEST();
+	
+	SUBTEST(subtest_construction);
+	SUBTEST(subtest_release);
+
+	PASS();
+};
